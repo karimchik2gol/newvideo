b3ee68a73a823440e4ea67a67e8da10fdd090714
Karim <ghetto-workout-ru@yandex.ru>
2017-03-13 23:35:03 +0200

    remove junk

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "plugins")
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "pages")
